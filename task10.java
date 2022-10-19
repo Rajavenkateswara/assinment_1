a617496ab327bb12e22ede6fb8fcec0f624e608e
Rajavenkateswara <115797671+Rajavenkateswara@users.noreply.github.com>
2022-10-19 10:40:49 -0400

    Add files via upload

---
+++ task10.java
@@ -0,0 +1,26 @@
+package Assignment_1;
+
+public class task10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//- Write a program which will break the current execution if it find “Selenium”
+		//Input – [“Java”,”JavaScript”,”Selenium”,”Python”,”Mukesh”]
+		
+		String name[]={"java","JavaScript","Selenium","Python","Mukesh"};
+		for(String name1:name ){
+			if(name1=="Selenium")
+				break;
+			
+			System.out.println(name1);
+			 }
+			
+		}
+		
+		
+		
+		
+		}
+	
+
+
---
+++ task_9.java
@@ -0,0 +1,23 @@
+package Assignment_1;
+
+public class task_9 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Write a program which will break the current execution if it find number 85
+		// Input – [12,34,66,85,900]
+		int num[]={12,34,66,85,900};
+		
+		for(int num1:num){
+			if(num1==85)
+				break;
+			
+				System.out.println("Given numbers " + num1);
+			}
+		
+				
+		}
+		
+	}
+
+
